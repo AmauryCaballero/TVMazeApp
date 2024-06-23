34eefd85a1881fdb7dd1032973407282b7b8f562
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-06-23 18:23:21 -0400

    feat: refactoring the api client

--- tv_maze_app/lib/core/network/api_client.dart
+++ tv_maze_app/lib/core/network/api_client.dart
@@ -1,22 +1,32 @@
 import 'package:dio/dio.dart';
+import '../errors/exceptions.dart';
 import 'api_response.dart';
+import 'network_info.dart';
 
 class ApiClient {
   final Dio _dio;
+  final NetworkInfo _networkInfo;
 
-  ApiClient(this._dio) {
+  ApiClient(this._dio, this._networkInfo) {
     _dio
       ..options.baseUrl = 'https://api.tvmaze.com'
       ..options.connectTimeout = const Duration(milliseconds: 5000)
       ..options.receiveTimeout = const Duration(milliseconds: 3000);
   }
 
-  Future<ApiResponse> get(String path, {Map<String, dynamic>? params}) async {
-    try {
-      final response = await _dio.get(path, queryParameters: params);
-      return ApiResponse.success(response.data);
-    } catch (error) {
-      return ApiResponse.failure(error as Exception?);
+  Future<ApiResponse<T>> request<T>(
+      {required String path,
+      required T Function(dynamic) fromJson,
+      Map<String, dynamic>? params}) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final response = await _dio.get(path, queryParameters: params);
+        return ApiResponse.success(fromJson(response.data));
+      } catch (error) {
+        return ApiResponse.failure(ServerException());
+      }
+    } else {
+      return ApiResponse.failure(NoConnectionException());
     }
   }
 }
