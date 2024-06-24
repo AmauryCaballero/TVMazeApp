c1afc340210cc64554d951ce0018c3ccf9e9d7af
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-06-23 22:49:44 -0400

    feat: create `SplashScreen`

---
+++ tv_maze_app/lib/presentation/pages/splash/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
