b33208d9f9ed1a4496aeb398ac61ad7923fa0b9e
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-06-23 22:51:00 -0400

    feat: create app router

--- tv_maze_app/lib/presentation/pages/splash/splash_screen.dart
+++ tv_maze_app/lib/presentation/pages/splash/splash_screen.dart
@@ -1,9 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
+  Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         color: Colors.red,
