486ba7935e475c66ea21d52b48f806e683b8dea8
Amaury Miguel Caballero <62359438+AmauryCaballero@users.noreply.github.com>
2024-06-24 15:04:34 -0400

    feat: adding lottie animation to splash screen

--- tv_maze_app/lib/core/utils/assets_extension.dart
+++ tv_maze_app/lib/core/utils/assets_extension.dart
@@ -1,4 +1,5 @@
 extension AssetsExtension on String {
   String get pngImage => 'assets/images/$this.png';
   String get jpgImage => 'assets/images/$this.jpg';
+  String get jsonLottie => 'assets/animations/$this.json';
 }
--- tv_maze_app/lib/presentation/pages/splash/splash_screen.dart
+++ tv_maze_app/lib/presentation/pages/splash/splash_screen.dart
@@ -2,13 +2,35 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tv_maze_app/core/utils/assets_extension.dart';
 
 import '../../blocs/splash/bloc/splash_bloc.dart';
 import '../../routes/app_router.dart';
 
 @RoutePage()
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +43,14 @@ class SplashScreen extends StatelessWidget {
             context.router.replace(HomeRoute(seriesList: state.seriesList));
           }
         },
-        child: const Scaffold(),
+        child: Scaffold(
+          body: Center(
+            child: Lottie.asset(
+              'tv_splash_animation'.jsonLottie,
+              controller: _controller,
+            ),
+          ),
+        ),
       ),
     );
   }
